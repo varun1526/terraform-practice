e4b598693bdb7b08d969f36df4015d8534282b10
varun.mittal <varun.mittal@roundcircle.tech>
2025-12-07 12:58:27 +0530

    update for vpc

---
+++ vpc/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    name   = "${var.environment}-vpc"
+    azs    = data.aws_availability_zones.available.names
+    private_subnets = var.private_cidr
+    public_subnets = var.public_cidr
+    enable_nat_gateway = true
+}
